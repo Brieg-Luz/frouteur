9e78ec5a8d05625741621c8c61476e4898eee197
Brieg Luz <briegluz@gmail.com>
2021-06-14 23:11:54 +0200

    changement du nom de licence

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bc779386c4ecf021cd84a4ecc62056f4efe08843',
+    'name' => 'philigentil/frouteur',
+  ),
+  'versions' => 
+  array (
+    'philigentil/frouteur' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bc779386c4ecf021cd84a4ecc62056f4efe08843',
+    ),
+  ),
+);
